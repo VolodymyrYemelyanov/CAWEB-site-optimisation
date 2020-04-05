f676bda23f109cda8a53703fb48f9130eb786c19
VladYem <volodymyem@gmail.com>
2020-04-05 05:37:11 +0200

    Mobile navigation fix; pop-up finished +responsive

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
+let pass1 = document.querySelector('#popUp-pass1');
+let pass2 = document.querySelector('#popUp-pass2');
+
+pass2.addEventListener('keyup', function () {
+  if (pass1.value !== pass2.value) {
+    pass2.style.borderBottom = '3px solid #ff7730';
+  } else {
+    pass2.style.borderBottom = '3px solid #55c57a';
+  }
+});
+
 function initMap() {
   // The location of Uluru
   var uluru = {
     lat: 43.694794,
-    lng: 7.260665
+    lng: 7.260665,
   };
   // The map, centered at Uluru
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: uluru
+    center: uluru,
   });
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({
     position: uluru,
-    map: map
+    map: map,
   });
 }
 
-$(document).ready(function() {
-  $('#btn-job-category').click(function() {
+$(document).ready(function () {
+  $('#btn-job-category').click(function () {
     var btnCategoryText = $('#btn-job-category-text').html();
     $('#job-category-hidden').toggleClass('job-category-hidden');
     if (btnCategoryText === 'See more') {
@@ -27,11 +38,11 @@ $(document).ready(function() {
     }
   });
 
-  $('.btn__link').click(function() {
+  $('.btn__link').click(function () {
     $('#popUp').removeClass('hide-popUp');
   });
 
-  $('.popUp__close').click(function() {
+  $('.popUp__close').click(function () {
     $('#popUp').addClass('hide-popUp');
   });
 });
